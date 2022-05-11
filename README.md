962c41c8b8f2f18b8d384e92adfd3db2c43c355a
= <=>
2022-05-11 14:16:43 +0200

    added colormaps

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 This repository contains a collection of colormaps that are useful when generating figures for
 presentations or papers by the Uppsala Social Robotics Lab. Available colormaps:
 
+![Showcase of available colormaps](imgs/colormaps.png)
+
 ## Installation
 
 ```
@@ -12,15 +14,43 @@ pip install usrl-colormaps
 
 ## Usage
 
+Inside of matplotlib:
+
 ```python
 import matplotlib.pyplot as plt
-import usrl-colormaps  # colormaps are registered automatically
+import matplotlib.cm as cm
+import usrl_colormaps  # colormaps are registered automatically
 
 fig = plt.figure()
 ax = fig.add_axes([0,0,1,1])
 langs = ['C', 'C++', 'Java', 'Python', 'PHP']
 students = [23,17,35,29,12]
-ax.bar(langs,students)
+ax.bar(langs,students, color=cm.get_cmap("usr_qualitative").colors)
+ax.grid(False)
 plt.show()
 
 ```
+
+![Matplotlib example plot](imgs/matplotlib_example.png)
+
+In seaborn:
+
+```python
+import seaborn as sns
+import usrl_colormaps  # colormaps are registered automatically
+
+
+sns.set_theme(style="whitegrid")
+diamonds = sns.load_dataset("diamonds")
+
+# Plot the distribution of clarity ratings, conditional on carat
+sns.displot(
+    data=diamonds,
+    x="carat", hue="cut",
+    kind="kde", height=6,
+    multiple="fill", clip=(0, None),
+    palette="usr_oranges",
+)
+```
+
+![Seaborn example plot](imgs/seaborn_example.png)
--- usrl_colormaps/__init__.py
+++ usrl_colormaps/__init__.py
@@ -1,1 +1,26 @@
 import matplotlib
+import numpy as np
+
+
+ORANGE = np.array([244,114,22, 255]) / 255 # orange
+RED = np.array([241,69,91, 255]) / 255 # red
+GREEN = np.array([151,211,33, 255]) / 255 # green
+BLUE = np.array([98,200,243, 255]) / 255  # blue
+OFF_WHITE = np.array([245, 245, 245, 255]) / 255
+WHITE = np.array([255, 255, 255, 255]) / 255
+
+usr_reds = matplotlib.colors.ListedColormap(np.linspace(OFF_WHITE, RED, 255), name="usr_reds")
+usr_blues = matplotlib.colors.ListedColormap(np.linspace(OFF_WHITE, BLUE, 255), name="usr_blues")
+usr_greens = matplotlib.colors.ListedColormap(np.linspace(OFF_WHITE, GREEN, 255), name="usr_greens")
+usr_oranges = matplotlib.colors.ListedColormap(np.linspace(OFF_WHITE, ORANGE, 255), name="usr_oranges")
+usr_diverge = matplotlib.colors.ListedColormap(np.vstack((np.linspace(BLUE, WHITE, 255), np.linspace(WHITE, RED, 255))), name="usr_diverging")
+usr_diverge2 = matplotlib.colors.ListedColormap(np.vstack((np.linspace(GREEN, WHITE, 255), np.linspace(WHITE, ORANGE, 255))), name="usr_diverging2")
+usr_qualitative = matplotlib.colors.ListedColormap((RED, BLUE, ORANGE, GREEN), name="usr_qualitative")
+
+matplotlib.cm.register_cmap(cmap=usr_reds)
+matplotlib.cm.register_cmap(cmap=usr_blues)
+matplotlib.cm.register_cmap(cmap=usr_greens)
+matplotlib.cm.register_cmap(cmap=usr_oranges)
+matplotlib.cm.register_cmap(cmap=usr_diverge)
+matplotlib.cm.register_cmap(cmap=usr_diverge2)
+matplotlib.cm.register_cmap(cmap=usr_qualitative)
